e5d0ffdae5cc90b15acec63dbb20825a2e619f43
Braulio Viveros <bobbyviveros9@gmail.com>
2024-04-30 21:38:25 -0600

    functions for initializing and displaying the game grid now implemented into the class

--- ttt.cpp
+++ ttt.cpp
@@ -6,3 +6,40 @@
 
 #include "ttt.h"
 
+void TTT::initialize_grid()
+{
+  int k = 1;
+  for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+      grid[i][j] = std::to_string(k);
+      k++;
+    }
+  }
+
+}
+
+void TTT::display_grid()
+{
+  std::cout << "Current Grid" << std::endl;
+  for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+      std::cout << grid[i][j];
+      if(j == 2){break;}
+      std::cout << " || ";
+    }
+    std::cout << std::endl;
+    if( i == 2){break;}
+    std::cout << "___ ___ ___" << std::endl;
+    std::cout << std::endl;
+  }
+}
+
+void TTT::get_players_names()
+{
+  std::cout << "Enter player 1's name: ";
+  std::cin >> player1;
+
+  std::cout << "Enter player 2's name: ";
+  std::cin >> player2;
+}
+
--- ttt.h
+++ ttt.h
@@ -3,27 +3,33 @@
  * class functions declared 
  */
 
-
 #ifndef TTT_H
 #define TTT_H
 
+#include <iostream>
 #include <string>
 
-using std::string;
-
-class TTT{
-    public:
-        /* member functions */
-        void display_grid();
+class TTT
+{
     private:
         /* member variables */
         // names of the players
-        string player1;
-        string player2;
+        std::string player1;
+        std::string player2;
         // pieces the players will use
-        char o_piece = 'o';
-        char x_piece = 'x';
+        const char o_piece;
+        const char x_piece;
+        // grid in which the game will be played
+        std::string grid[3][3];
+    public:
+        /* member functions */
+        /* constructor with initilization list */
+        TTT() : player1(""), player2(""), o_piece('o'), x_piece('x'){}
 
+        /* regular member functions */
+        void initialize_grid();
+        void display_grid();
+        void get_players_names();
 };
 
 
